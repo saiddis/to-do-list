a35a9c89a4154603eca850296010c130b3e06de8
saiddis <saiddisruptive@gmail.com>
2024-06-29 17:03:09 +0500

    modified:   images/plus.svg 	modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-class TaskTracker {
+class ToDoList {
     constructor() {
         this.weekDays = ['Sunday', 'Monday', 'Tuesday',
             'Wednesday','Thursday', 'Friday', 'Saturday'];
         this.months = ['January', 'February', 'March', 
             'April','May', 'June', 'July', 'August','September',
             'October', 'Novermber', 'December'];
-        this.allTaskTrackers = new Map();
+        this.cachedTaskContainers = new Map();
 
         this.previousButton = document.querySelector('.previous-button');
         this.nextButton = document.querySelector('.next-button');
@@ -19,28 +19,83 @@ class TaskTracker {
         this.date = new Date();
         this.taskOrder = 0;
         
-        this.addTaskButton.style.left = document.documentElement.clientWidth / 2 - this.addTaskButton.clientWidth / 2 + 'px';
-        this.addTaskButton.style.top = document.documentElement.clientHeight / 1.25 + 'px'; 
+        /* this.addTaskButton.style.left = document.documentElement.clientWidth / 2 - this.addTaskButton.clientWidth / 2 + 'px'; */
+        /* this.addTaskButton.style.top = document.documentElement.clientHeight / 1.25 + 'px';  */
         this.taskContainer.style.top = this.dateBar.clientHeight + 'px';
         this.dateNode.innerHTML = `${this.weekDays[this.date.getDay()]}, ${this.months[this.date.getMonth()]} ${this.date.getDate()}`;
         
         this.addTask = this.addTask.bind(this);
         this.changeDate = this.changeDate.bind(this);
         this.onCheckBox = this.onCheckBox.bind(this);
+        this.inputNavingation = this.inputNavingation.bind(this);
 
-        this.addTaskButton.onclick = (event) => {
+        this.addTaskButton.onclick = async () => {
             if (this.taskList.childNodes &&
                 this.taskList.lastElementChild?.lastElementChild.value.trim() == false) {
                     this.taskList.lastElementChild.lastElementChild.focus();
                     return false;
             } else {
-                this.addTask(event);
+                let li = await Promise.resolve(this.addTask());
+                this.taskList.append(li); 
+                li.lastElementChild.focus();
+        
             }
         };
         this.dateBar.onclick = event => {
             this.changeDate(event);
         };
     };
+
+    async inputNavingation(event, input) {
+        
+
+        switch (event.key) {
+            case 'Enter':
+                if (!input.value.trim()) {
+                    return false;
+                }
+
+                if (this.taskList.lastElementChild?.lastElementChild.value.trim() == false) {
+                    this.taskList.lastElementChild.lastElementChild.focus();
+                    return false;
+                }
+
+                const newLi = await Promise.resolve(this.addTask());
+                input.parentElement.insertAdjacentElement('afterend', newLi);
+                newLi.lastElementChild.focus();
+                break;
+            case 'Backspace':
+                if (!input.value) {
+                    const li = input.parentElement;
+                    const previousLi = li.previousElementSibling;
+                    li.remove();
+                    if (!previousLi) return;
+    
+                    previousLi.lastElementChild.value += ' ';
+                    previousLi.lastElementChild.focus();
+                } 
+                break;
+            case 'ArrowUp':
+                const previousLi = input.parentElement.previousElementSibling;
+                if (previousLi) {
+                    previousLi.lastElementChild.focus();
+                    setTimeout(() => {
+                        previousLi.lastElementChild.selectionStart = previousLi.lastElementChild.selectionEnd = input.selectionStart;
+                    });
+                    
+                }
+                break;
+            case 'ArrowDown':
+                const nextLi = input.parentElement.nextElementSibling;
+                if (nextLi) {
+                    nextLi.lastElementChild.focus()
+                    setTimeout(() => {
+                        nextLi.lastElementChild.selectionStart = nextLi.lastElementChild.selectionEnd = input.selectionStart;
+                    });
+                }
+                break;
+        }
+    }
 
     changeDate(event) {
         let target = event.target.closest('button');
@@ -55,20 +110,25 @@ class TaskTracker {
 
         let newDate = `${this.weekDays[this.date.getDay()]}, ${this.months[this.date.getMonth()]} ${this.date.getDate()}`;
 
-        if (!this.allTaskTrackers.has(this.dateNode.innerHTML)) {
-            this.allTaskTrackers.set(this.dateNode.innerHTML, this.taskContainer);
+        if (!this.cachedTaskContainers.has(this.dateNode.innerHTML)) {
+            this.cachedTaskContainers.set(this.dateNode.innerHTML, this.taskContainer);
         }
-        if (this.allTaskTrackers.has(newDate)) {
-            this.taskContainer.replaceWith(this.allTaskTrackers.get(newDate));
-            this.taskContainer = this.allTaskTrackers.get(newDate);
+        if (this.cachedTaskContainers.has(newDate)) {
+            this.taskContainer.replaceWith(this.cachedTaskContainers.get(newDate));
+            this.taskContainer = this.cachedTaskContainers.get(newDate);
+            this.taskList = this.taskContainer.firstElementChild;
+            this.completedTaskList = this.taskContainer.lastElementChild;
+            this.titleCompleted = this.completedTaskList.firstElementChild;
         } else {
             let newTaskContainer = this.taskContainer.cloneNode(false);
             let newTaskList = this.taskList.cloneNode(false);
             let newCompletedTaskList = this.completedTaskList.cloneNode(false);
             let newTitleCompleted = this.titleCompleted.cloneNode(true);
+            newCompletedTaskList.hidden = true;
 
             newCompletedTaskList.appendChild(newTitleCompleted);
-            newTaskContainer.appendChild(newTaskList, newCompletedTaskList);
+            newTaskContainer.appendChild(newTaskList);
+            newTaskContainer.appendChild(newCompletedTaskList);
             this.taskContainer.replaceWith(newTaskContainer);
 
             this.taskContainer = newTaskContainer;
@@ -96,51 +156,25 @@ class TaskTracker {
 
         checkBox.className = 'check-box';
         checkBox.src = uncheckedBoxUrl;
-        li.setAttribute('order', ++this.taskOrder);
+
         li.append(checkBox);
         li.append(input);
-        this.taskList.append(li);
-        input.focus();
-
         checkBox.onclick = event => this.onCheckBox(event, uncheckedBoxUrl, checkedBoxUrl);
-    
         input.onkeydown = event => {
-            switch (event.key) {
-                case 'Enter':
-                    if (!input.value.trim()) {
-                        return;
-                    }
-                    this.addTask();
-                    break;
-                case 'Backspace':
-                    if (!input.value.trim()) {
-                        let previousLi = li.previousElementSibling;
-                        li.remove();
-                        if (!previousLi) return;
-        
-                        previousLi.lastElementChild.value += ' ';
-                        previousLi.lastElementChild.focus();
-                    } 
-                    break;
-                case 'ArrowUp':
-                    if (li.previousElementSibling) {
-                        li.previousElementSibling.lastElementChild.focus();
-                    }
-                    break;
-                case 'ArrowDown':
-                    if (li.nextElementSibling) {
-                        li.nextElementSibling.lastElementChild.focus()
-                    }
-                    break;
-            }
+            this.inputNavingation(event, input);
         };
+
+        return li;
     }; 
 
     onCheckBox(event, uncheckedBoxUrl, checkedBoxUrl) {
         const checkBox = event.target.closest('img');
-        if (!checkBox) return;
+   
+        if (!checkBox.nextElementSibling.value.trim()) {
+            return false;
+        }
 
-        const li = checkBox.parentElement;    
+        const li = checkBox.parentElement;
         if (checkBox.src == uncheckedBoxUrl) {
             checkBox.src = checkedBoxUrl;
             checkBox.nextSibling.style.opacity = '0.3';
@@ -151,7 +185,7 @@ class TaskTracker {
         } else {
             checkBox.src = uncheckedBoxUrl;
             checkBox.nextSibling.style.opacity = '1';
-            this.taskList.append(li);
+            this.taskList.prepend(li);
             if (this.titleCompleted == this.completedTaskList.lastElementChild) {
                 this.completedTaskList.hidden = true;
             }
@@ -159,4 +193,4 @@ class TaskTracker {
     };
 }; 
 
-let tracker = new TaskTracker();
+let toDoList = new ToDoList();
