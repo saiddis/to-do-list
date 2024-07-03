9069d052855d4adb3c5decc6254fecf37562c022
saiddis <saiddisruptive@gmail.com>
2024-07-03 15:20:12 +0500

    deleted:    images/arrow-next.svg 	deleted:    images/arrow-prev.svg 	deleted:    images/checked-box.svg 	deleted:    images/plus.svg 	deleted:    images/unchecked-box.svg 	modified:   index.html     modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ class ToDoList {
             'October', 'Novermber', 'December'];
         this.cachedTaskContainers = new Map();
 
-        this.previousButton = document.querySelector('.previous-button');
-        this.nextButton = document.querySelector('.next-button');
+        this.arrowLeft = document.querySelector('.arrow-left');
+        this.arrowRight = document.querySelector('.arrow-right');
         this.taskContainer = document.querySelector('.task-container');
         this.taskList = this.taskContainer.firstElementChild;
         this.completedTaskList = this.taskContainer.lastElementChild;
@@ -18,7 +18,17 @@ class ToDoList {
         this.titleCompleted = document.querySelector('.title-completed');
         this.date = new Date();
         this.taskOrder = 0;
+        this.checkBoxSvg = `
+        <svg class="check-box" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <rect fill="#f8f9fa" stroke-width="1" stroke="#adb5bd" x="0.5" y="0.5" width="23" height="23" rx="3"/>
+        <path class="tick" fill="#212529" d="M19.707,7.293a1,1,0,0,0-1.414,0L10,15.586,6.707,12.293a1,1,0,0,0-1.414,1.414l4,4a1,1,0,0,0,1.414,0l9-9A1,1,0,0,0,19.707,7.293Z"/>
+        </svg>`;
+        this.crossSvg = `
+        <svg class="cross" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M19 5L5 19M5.00001 5L19 19" stroke="#000000" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>`;
         
+        this.addTaskButton.style.left = document.documentElement.clientWidth / 2 - this.addTaskButton.clientWidth / 2 + 'px';
         this.taskContainer.style.top = this.dateBar.clientHeight + 'px';
         this.dateNode.innerHTML = `${this.weekDays[this.date.getDay()]}, ${this.months[this.date.getMonth()]} ${this.date.getDate()}`;
         
@@ -46,7 +56,6 @@ class ToDoList {
 
     async inputNavingation(event, input) {
         
-
         switch (event.key) {
             case 'Enter':
                 if (!input.value.trim()) {
@@ -96,13 +105,11 @@ class ToDoList {
     }
 
     changeDate(event) {
-        let target = event.target.closest('button');
-        if (!target) return;
+        let target = event.target.closest('svg');
         
-        this.taskOrder = 0;
-        if (target.className == 'next-button') {
-           this.date = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate() + 1);
-        } else if (target.className == 'previous-button') {
+        if (target == this.arrowRight) {
+            this.date = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate() + 1);
+        } else if (target == this.arrowLeft) {
             this.date = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate() - 1);
         }
 
@@ -141,47 +148,46 @@ class ToDoList {
     async addTask() {
         const li = document.createElement('li');
         const input = document.createElement('input');
-        const checkBox = document.createElement('img');
+        li.insertAdjacentHTML('afterbegin', this.crossSvg);
+        li.insertAdjacentHTML('beforeend', this.checkBoxSvg);
+        const cross = li.firstElementChild;
+        const checkBox = cross.nextElementSibling;
+        checkBox.lastElementChild.style.opacity = '0';
         
-        let uncheckedBox = await fetch('images/unchecked-box.svg');
-        let uncheckedBoxBlob = await uncheckedBox.blob();
-        let uncheckedBoxUrl = URL.createObjectURL(uncheckedBoxBlob);
-
-        let checkedBox = await fetch('images/checked-box.svg');
-        let checkedBoxBlob = await checkedBox.blob();
-        let checkedBoxUrl = URL.createObjectURL(checkedBoxBlob);
-
-
-        checkBox.className = 'check-box';
-        checkBox.src = uncheckedBoxUrl;
-
-        li.append(checkBox);
         li.append(input);
-        checkBox.onclick = event => this.onCheckBox(event, uncheckedBoxUrl, checkedBoxUrl);
-        input.onkeydown = event => {
-            this.inputNavingation(event, input);
+
+        checkBox.onclick = event => this.onCheckBox(event);
+        input.onkeydown = event => this.inputNavingation(event, input);
+        cross.onclick = () => {
+            if (li.parentElement == this.completedTaskList &&
+                this.completedTaskList.children.length == 2) {
+                this.completedTaskList.hidden = true;
+            }
+            li.remove();
         };
 
         return li;
     }; 
 
-    onCheckBox(event, uncheckedBoxUrl, checkedBoxUrl) {
-        const checkBox = event.target.closest('img');
-   
+    onCheckBox(event) {
+        const checkBox = event.target.closest('svg');
+
         if (!checkBox.nextElementSibling.value.trim()) {
+            checkBox.nextElementSibling.focus();
             return false;
         }
 
         const li = checkBox.parentElement;
-        if (checkBox.src == uncheckedBoxUrl) {
-            checkBox.src = checkedBoxUrl;
+
+        if (checkBox.lastElementChild.style.opacity == '0') {
+            checkBox.lastElementChild.style.opacity = '1';
             checkBox.nextSibling.style.opacity = '0.3';
             if (this.completedTaskList.hidden) {
                 this.completedTaskList.hidden = false;
             }
             this.completedTaskList.append(li);
         } else {
-            checkBox.src = uncheckedBoxUrl;
+            checkBox.lastElementChild.style.opacity = '0';
             checkBox.nextSibling.style.opacity = '1';
             this.taskList.prepend(li);
             if (this.titleCompleted == this.completedTaskList.lastElementChild) {
